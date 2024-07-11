ce1f92773b1667d9fa2b0e91396035dff44afc93
IleanaZubascu <zubascuile30@yahoo.com>
2024-07-11 16:27:39 +0300

    changed package name

--- src/main/java/com/example/demo/MODEL/User.java
+++ src/main/java/com/example/demo/MODEL/User.java
@@ -1,8 +1,6 @@
-package com.example.demo.MODEL;
+package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
