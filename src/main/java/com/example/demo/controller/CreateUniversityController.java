ec2e80466a00546bb957856429f30ec051d54084
IleanaZubascu <zubascuile30@yahoo.com>
2024-07-16 11:10:55 +0300

    peer-10

---
+++ src/main/java/com/example/demo/controller/CreateUniversityController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.service.UniversityService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class UniversityController {
+
+    @
+}
