8d0d89a747b85375cde617e23cae0660f42df86a
IleanaZubascu <zubascuile30@yahoo.com>
2024-07-25 14:45:45 +0300

    Fixed the issues

--- src/main/java/com/ghsbm/group/peer/colab/domain/school/persistence/model/CityEntity.java
+++ src/main/java/com/ghsbm/group/peer/colab/domain/school/persistence/model/CityEntity.java
@@ -8,9 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
