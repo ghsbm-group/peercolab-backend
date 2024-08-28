a67e6e404a7d683818929de0f1e7b8957da0d78e
IleanaZubascu <zubascuile30@yahoo.com>
2024-08-28 14:43:34 +0300

    Fixed sonarcloud issue

--- src/main/java/com/ghsbm/group/peer/colab/domain/classes/controller/model/dto/FolderInfoDTO.java
+++ src/main/java/com/ghsbm/group/peer/colab/domain/classes/controller/model/dto/FolderInfoDTO.java
@@ -1,6 +1,5 @@
 package com.ghsbm.group.peer.colab.domain.classes.controller.model.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
