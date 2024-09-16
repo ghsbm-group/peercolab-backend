b19bf87f64b76a20a203ca24738ba599df79b6d1
Cristian Sechel <sechelc@ghsbmgroup.com>
2024-09-16 14:57:33 +0300

    removed diacritics from cities

---
+++ src/main/resources/db/migration/mock/V9.9.12__change_city_names.sql
@@ -0,0 +1,42 @@
+UPDATE city
+set name='Pitesti'
+where name = 'Pitești';
+UPDATE city
+set name='Bacau'
+where name = 'Bacău';
+UPDATE city
+set name='Brasov'
+where name = 'Brașov';
+UPDATE city
+set name='Bucuresti'
+where name = 'București';
+UPDATE city
+set name='Constanta'
+where name = 'Constanța';
+UPDATE city
+set name='Galati'
+where name = 'Galați';
+UPDATE city
+set name='Iasi'
+where name = 'Iași';
+UPDATE city
+set name='Petrosani'
+where name = 'Petroșani';
+UPDATE city
+set name='Pitesti'
+where name = 'Pitești';
+UPDATE city
+set name='Ploiesti'
+where name = 'Ploiești';
+UPDATE city
+set name='Targoviste'
+where name = 'Târgoviște';
+UPDATE city
+set name='Targu-Jiu'
+where name = 'Târgu-Jiu';
+UPDATE city
+set name='Targu-Mures'
+where name = 'Târgu-Mureș';
+UPDATE city
+set name='Timisoara'
+where name = 'Timișoara';
