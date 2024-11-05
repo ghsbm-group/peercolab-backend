f7e3b35914a2e7896dd7eec80e34e614c6cc4754
IleanaZubascu <115020198+IleanaZubascu@users.noreply.github.com>
2024-11-05 13:33:54 +0200

    Added user id in response /api/all-authority-requests (#57)

--- src/main/java/com/ghsbm/group/peer/colab/domain/security/controller/model/UserMapperController.java
+++ src/main/java/com/ghsbm/group/peer/colab/domain/security/controller/model/UserMapperController.java
@@ -2,6 +2,7 @@ package com.ghsbm.group.peer.colab.domain.security.controller.model;
 
 import com.ghsbm.group.peer.colab.domain.security.core.model.UserAuthorityRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * {@link Mapper} Contains methods for transforming core domain objects into dto's and back.
@@ -12,6 +13,9 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapperController {
+
+  @Mapping(source = "id", target = "userId")
+  UserAuthorityRequestResponse map(UserAuthorityRequest userAuthorityRequest);
 
   List<UserAuthorityRequestResponse> userAuthorityRequestResponsesFromUserAuthorityRequest(
       List<UserAuthorityRequest> userAuthorityRequests);
